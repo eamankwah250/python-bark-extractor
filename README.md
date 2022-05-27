f4cb37fbdb7f633f329852703fcfdbeece4c5112
Heechul Shin <heeshin174@gmail.com>
2022-05-26 19:22:27 -0500

    extract first client

--- README.md
+++ README.md
@@ -1,10 +1,36 @@
 # BARK-Extractor
 
+## Dependencies
+
+- Chrome browser
+- `python --version`: Python 3.8.5
+- `pip show selenium`: Version: 3.141.0
+
 ## Database_Schema
 
-To see the Epibark_Database_Schema
+To see the Epibark_Database_Schema:
 
 1. open www.draw.io
-2.
+2. Drag and drop `./Database_Schema/Epibark_Database_Schema.drawio` file
 
 ## SetUp
+
+1. Download source code by:
+   - `$ git clone https://github.com/heeshin174/bark-extractor`
+   - Download Zip file
+2. Download latest **Python** programming language
+   - [Python Download Link](https://www.python.org/downloads/)
+3. Install dependencies for project
+   - `$ pip install selenium`
+   - `$ pip install selenium==4.0.0`
+4. Download **Chrome browser** on your local computer
+5. Check your Chrome browser Version
+   - Settings -> About Chrome
+6. Google **chrome driver** and download the correct chrome driver that matches your current chrome version.
+   - [ChromeDriver Download Link](https://chromedriver.chromium.org/downloads)
+7. Replace `./chromedriver.exe` to your downloaded `./chromedriver.exe`.
+
+## Usage
+
+Excute Program = `python bark_extractor.py`
+Terminate Program = `Ctrl + C`
--- bark_extractor.py
+++ bark_extractor.py
@@ -1,10 +1,45 @@
-# pip install openpyxl selenium
+from multiprocessing.connection import wait
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.ui import WebDriverWait
+import time
 
-# change chrome driver to your current chrome version
+
+def wait_until(xpath):
+    WebDriverWait(browser, 30).until(
+        EC.presence_of_all_elements_located((By.XPATH, xpath)))
+
+
+# ****change chrome driver to your current chrome version**********
 browser = webdriver.Chrome('./chromedriver.exe')
-url = 'https://www.bark.com'
+url = 'https://www.bark.com/en/us/login/'
 browser.get(url)
-# signin = browser.find_element(By.XPATH, '//a[text() = "Try for free "]')
-# signin.click()
+
+# Login
+email = browser.find_element(
+    By.ID, "email").send_keys('barkleads@epibuild.com')
+password = browser.find_element(By.ID, "password").send_keys('Thanku2Rob')
+login = browser.find_element(By.XPATH, '//button[text() = "Log in"]').click()
+
+# Click Leads
+wait_until('//a[text() = "Leads"]')
+leadsButton = browser.find_element(By.XPATH, '//a[text() = "Leads"]').click()
+
+try:
+    notification_text = '//u[text() = "{}"]'.format(
+        "I don't want web notifications")
+    wait_until(notification_text)
+    notification = browser.find_element(By.XPATH, notification_text).click()
+except:
+    print('error occurs')
+
+
+# TODO
+# Iterate all clients
+lead = browser.find_element(
+    By.XPATH, '//div[@class="project-details-project-container"]')
+image = browser.find_element(
+    By.XPATH, '//img[@class="img-fluid rounded"]')
+print(lead.text)
+print(image.get_attribute('src'))
